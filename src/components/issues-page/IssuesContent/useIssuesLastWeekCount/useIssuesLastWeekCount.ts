efcfde869b6b4ad683495e1fbe5d8ff0cdd085ca
Paulo Endoh <paulo.endoh@gmail.com>
2023-04-02 13:21:07 -0300

    issues count

--- src/components/issues-page/IssuesContent/useIssuesLastWeekCount/useIssuesLastWeekCount.ts
+++ src/components/issues-page/IssuesContent/useIssuesLastWeekCount/useIssuesLastWeekCount.ts
@@ -1,15 +1,23 @@
 import { DateTime } from 'luxon'
 import { useMemo } from 'react'
 import { useIssuesQuery } from '../../../../hooks/react-query/monerate/issue/useIssuesQuery'
+import useIssueFilterStore from '../../../../hooks/zustand/useIssueFilterStore'
 
 export const useIssuesLastWeekCount = () => {
   const { data: issues } = useIssuesQuery()
+
+  const { filterIssueLabelIds } = useIssueFilterStore()
 
   const issuesLastWeekCount = useMemo(() => {
     let count = 0
 
     for (const i of issues || []) {
       if (!i.solvedAt) continue
+      if (
+        filterIssueLabelIds.length > 0 &&
+        !i.labels.some((l) => filterIssueLabelIds.includes(l.id))
+      )
+        continue
 
       const date = DateTime.fromISO(i.solvedAt)
       const diff = DateTime.local().diff(date, 'days').days
@@ -20,7 +28,7 @@ export const useIssuesLastWeekCount = () => {
     }
 
     return count
-  }, [issues])
+  }, [issues, filterIssueLabelIds])
 
   return issuesLastWeekCount
 }
