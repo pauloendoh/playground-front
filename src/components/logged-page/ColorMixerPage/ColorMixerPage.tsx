2e53f387f4d52b265dd25b9534b33a9774ce90f0
Paulo Endoh <paulo.endoh@gmail.com>
2024-01-15 02:42:05 -0300

    test zoom fixed

--- src/components/logged-page/ColorMixerPage/ColorMixerPage.tsx
+++ src/components/logged-page/ColorMixerPage/ColorMixerPage.tsx
@@ -34,7 +34,7 @@ const ColorMixerPage = (props: Props) => {
 
     // add this to
     const meta =
-      '<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1"/>'
+      '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">'
     document.head.insertAdjacentHTML('beforeend', meta)
 
     return () => {
